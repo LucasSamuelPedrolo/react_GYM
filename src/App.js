f06507d5c096510e0480fc0d722e5b60457ad166
lucas Pedrolo <lucassamuelpedrolo@gmail.com>
2023-10-25 23:26:50 -0300

    aprendendo react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: []
+  }
+
+  loadPosts = async () => {
+    const connectPost = fetch('https://jsonplaceholder.typicode.com/posts');
+    const connectImg = fetch('https://jsonplaceholder.typicode.com/photos');
+    const [posts, photos] = await Promise.all([connectPost, connectImg]);
+    const postsJson = await posts.json();
+    const imgJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: imgJson[index].url }
+    })
+
+    this.setState({ posts: postsAndPhotos });
+
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <section className='container'>
+        <div className="App">
+          {posts.map(post => (
+            <div className='post'>
+              <img src={post.cover} alt={post.title}></img>
+              <div key={post.id} className='post-content'>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
+
+
 
 export default App;
